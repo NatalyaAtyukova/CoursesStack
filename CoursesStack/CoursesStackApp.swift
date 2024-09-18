13370993cf2e469db23fb514a30176dd4f1cd3c8
NatalyaAtyukova <atukowa.natalya@yandex.ru>
2024-09-18 16:30:45 +0300

    Initial Commit

---
+++ CoursesStack/CoursesStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoursesStackApp.swift
+//  CoursesStack
+//
+//  Created by Наталья Атюкова on 18.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct CoursesStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
